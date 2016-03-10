af70632d293d30747f2bf2b3093ea570a64ecf5c
EngMRihan <eng.mohammadrihan@gmail.com>
2016-03-10 19:53:57 +0200

    first code

--- POM/searchPage.py
+++ POM/searchPage.py
@@ -2,10 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 
 class SearchPageObject(webdriver):
-    Searchbutton = webdriver.find_element_by_class_name("header_user_text")
-    webdriver.find_element_by_name("email").send_keys("csadek")
-    webdriver.find_element_by_name("mot_passe").send_keys("ZAQ!cde3")
-    webdriver.find_element_by_class_name("btn-success").click()
+    Searchbutton = webdriver.find_element_by_class_name("btn-header_search")
+
 
     def test_search_products(self ):
         self.Searchbutton.click();
--- Tests/searchTestCase.py
+++ Tests/searchTestCase.py
@@ -1,20 +1,10 @@
 import unittest
 import POM
+import Tests.BaseTestCase
 
 
-class searchTests(unittest.TestCase,):
-    # navigate to the application home page
-    self.driver.get("http://10.1.22.67/Jamaica/")
-    self.driver.find_element_by_class_name("header_user_text").click()
-    self.driver.find_element_by_name("email").send_keys("csadek")
-    self.driver.find_element_by_name("mot_passe").send_keys("ZAQ!cde3")
-    self.driver.find_element_by_class_name("btn-success").click()
+class searchTests(BaseTestCase):
 
-    def test_search_products(self):
-        select_category = Select(self.driver.find_element_by_id("search_category"))
-        select_category.select_by_index(1)
-        self.driver.find_element_by_class_name("btn-header_search").click()
-        self.driver.find_element_by_partial_link_text ("trouser").click()
-        self.driver.find_element_by_name("Add to cart").click()
+
 
 
