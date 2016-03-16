bb4122d8af8ddbfccc0d060eb4d7a1d5c8a86486
csadek <ZAQ!cde3>
2016-03-16 22:14:50 +0200

    rephrase the file

--- Tests/BaseTestCase.py
+++ Tests/BaseTestCase.py
@@ -2,15 +2,14 @@ import unittest
 from selenium import webdriver
 
 
-#BaseTestCase class, which will provide us with the setUp() and tearDown() methods
+# BaseTestCase class, which will provide us with the setUp() and tearDown() methods
 # so that we don't need to write these for each test class we create.
-
 class BaseTestCase(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(30)
         self.driver.maximize_window()
-       # navigate to the application home page
+        # navigate to the application home page
         self.driver.get('http://10.1.22.67/jamaica/')
 
     def tearDown(self):
