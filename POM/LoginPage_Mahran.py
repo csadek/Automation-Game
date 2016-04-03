2ca12b1a4c2cdf0cec03b3c47ffa71df3786ef0c
EngMRihan <eng.mohammadrihan@gmail.com>
2016-04-03 13:54:42 +0200

    4/3 Login Page Tests Done successfully

--- POM/LoginPage_Mahran.py
+++ POM/LoginPage_Mahran.py
@@ -11,6 +11,7 @@ class LoginPage(BasePageObject):
     PASSWORD = (By.CSS_SELECTOR, 'div:nth-child(2) > form > table > tbody > tr:nth-child(2) > td:nth-child(2) > input')
     LOGIN_BTN = (By.CSS_SELECTOR, 'tr:nth-child(3) > td > p > input.btn.btn-primary')
     Error_LBL = (By.CSS_SELECTOR, 'tr:nth-child(1) > td:nth-child(2) > div.alert.alert-danger.fade.in')
+    Error_LBL2 = (By.CSS_SELECTOR, 'tr:nth-child(2) > td:nth-child(2) > div.alert.alert-danger.fade.in')
 
     def __init__(self, driver):
         super(LoginPage, self).__init__(driver)
@@ -20,6 +21,14 @@ class LoginPage(BasePageObject):
         self.driver.find_element(*LoginPage.PASSWORD).send_keys(password)
         self.driver.find_element(*LoginPage.LOGIN_BTN).click()
         error_LBL = self.driver.find_element(*LoginPage.Error_LBL)
+        self.assertTrue(error_LBL.is_displayed())
+
+
+    def login_with_Invalid_credentials2(self, username, password):
+        self.driver.find_element(*LoginPage.USERNAME).send_keys(username)
+        self.driver.find_element(*LoginPage.PASSWORD).send_keys(password)
+        self.driver.find_element(*LoginPage.LOGIN_BTN).click()
+        error_LBL = self.driver.find_element(*LoginPage.Error_LBL2)
         self.assertTrue(error_LBL.is_displayed())
 
     def login_with_valid_credentials(self, username, password):
--- Tests/Login_T.py
+++ Tests/Login_T.py
@@ -10,3 +10,24 @@ class Login_T(BaseTestCase):
 
         if __name__ == '__main__':
             unittest.main()
+
+    def test_login_incorrect_email(self):
+       Login_Obj = LoginPage_Mahran
+       Login_Obj.LoginPage.login_with_Invalid_credentials(self,'test123@email.com', 'password')
+
+       if __name__ == '__main__':
+            unittest.main()
+
+    def test_login_blank_password(self):
+       Login_Obj = LoginPage_Mahran
+       Login_Obj.LoginPage.login_with_Invalid_credentials2(self,'test@email.com', '')
+
+       if __name__ == '__main__':
+            unittest.main()
+
+    def test_login_blank_email(self):
+       Login_Obj = LoginPage_Mahran
+       Login_Obj.LoginPage.login_with_Invalid_credentials(self,'', 'password')
+
+       if __name__ == '__main__':
+            unittest.main()
