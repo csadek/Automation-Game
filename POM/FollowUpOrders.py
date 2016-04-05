f7cbdd3bb8b81246846e43acebaacf38cdd2e821
csadek <ZAQ!cde3>
2016-04-05 12:31:30 +0200

    rephrase the file

--- POM/FollowUpOrders.py
+++ POM/FollowUpOrders.py
@@ -3,14 +3,14 @@ from selenium.webdriver.common.by import By
 from Utilities.BasePage_Mahran import BasePageObject
 
 
-class ManageBillingAddress(BasePageObject):
+class FollowUpOrders(BasePageObject):
     """ this class represent login page elements manipulations and functions"""
 
     # Locators
 # Login - follow up orders - click on item - check its details
 
     def __init__(self, driver):
-        super(ManageBillingAddress, self).__init__(driver)
+        super(FollowUpOrders, self).__init__(driver)
 
 
 
