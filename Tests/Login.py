a3f2531f3bf52c7285a5fcaf7b29ac6d7b8fe26c
csadek <ZAQ!cde3>
2016-04-05 23:06:21 +0200

    rephrase the file

---
+++ Tests/Login.py
@@ -0,0 +1,22 @@
+from POM.BaseTestCase import BaseTestCase
+from POM.LoginPage import LoginPage
+
+
+class Login(BaseTestCase):
+
+   # def __init__(self):
+     #   self = LoginPage.self
+
+    def test_login_incorrect_email(self):
+
+        LoginPage.login_with_Invalid_credentials(self, 'test123@email.com', 'password')
+
+    def test_login_blank_password(self):
+        LoginPage.login_with_Invalid_credentials2(self, 'test@email.com', '')
+
+    def test_login_blank_email(self):
+        login_obj = LoginPage()
+        LoginPage.login_with_Invalid_credentials(self, '', 'password')
+
+    def test_login_valid(self):
+        LoginPage.login_with_valid_credentials(self, 'eng.mohammadrihan@gmail.com', 'h0tr1ngG')
