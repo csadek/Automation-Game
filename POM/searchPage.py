e571e1f2db89d5ec53af25f20373a34a2a912379
jihadMohamed <jmohamed@integrant.com>
2016-04-06 20:04:34 +0200

    Search with vaild data

--- POM/searchPage.py
+++ POM/searchPage.py
@@ -1,17 +1,25 @@
 from selenium.webdriver.common.by import By
 from POM.BaseTestCase import BaseTestCase
+from selenium.webdriver.support.ui import Select
+
 
 class SearchPage(BaseTestCase):
 
+    SearchBox = (By.NAME,'search')
     CategoryList = (By.ID,'search_category')
     SearchButton =  (By.CLASS_NAME,'btn-header_search')
     SearchResult= (By.PARTIAL_LINK_TEXT,"trouser")
     AddToCardBtn = (By.NAME,'Add to cart')
 
-    def search_vaild_Data(self):
+    def search_vaild_Data(self,ProductName,CategoryName):
 
-        select_category=self.driver.find_element(*SearchPage.CategoryList)
-        select_category.select_by_index(1)
-        self.driver.find_element_by_class_name("btn-header_search").click()
-        self.driver.find_element_by_partial_link_text ("trouser").click()
-        self.driver.find_element_by_name("Add to cart").click()
+        #Product Name
+        self.driver.find_element(*SearchPage.SearchBox).send_keys(ProductName)
+        #find Category List On Page
+        selected_category=self.driver.find_element(*SearchPage.CategoryList)
+        selected_category.select_by_name(CategoryName)
+
+        #click on GO to find results
+        self.driver.find_element(*SearchPage.SearchButton).click()
+
+
