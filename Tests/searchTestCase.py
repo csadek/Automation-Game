66bdb640d4daa487c3630de5ba5c97fbdf0f2df1
jihadMohamed <jmohamed@integrant.com>
2016-04-06 19:44:13 +0200

    searchTestCase 4-6

--- Tests/searchTestCase.py
+++ Tests/searchTestCase.py
@@ -1,4 +1,8 @@
-import unittest
-import POM
-import Tests.BaseTestCase
+from POM.BaseTestCase import BaseTestCase
+from POM.searchPage import SearchPage
+
+class searchTests(BaseTestCase):
+
+    def test_search_products(self):
+        SearchPage.search_vaild_Data(self,'trouser','Clothing')
 
