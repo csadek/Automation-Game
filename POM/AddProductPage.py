b91646747ccbfe368aaa488694eeef9c4ee50ef2
csadek <ZAQ!cde3>
2016-04-14 00:41:20 +0300

    rephrase the file

--- POM/AddProductPage.py
+++ POM/AddProductPage.py
@@ -6,17 +6,20 @@ class AddProductPage(BaseTestCase):
 
     """ this class represents add product page elements manipulations and functions"""
 
+    # Navigators
+    admin_button = (By.CSS_SELECTOR,'#main_content > div:nth-child(1) > div > div > div.middle_column_repeat > div > a.btn.btn-warning.pull-right')
+    add_product_link = (By.CSS_SELECTOR,'a[href^=\"http://10.1.22.67/Jamaica/administrer/produits.php?mode=ajout\"]')
+
     # Locators
-    AddProductMenu = By.CSS_SELECTOR("a[href^=\"http://10.1.22.67/Jamaica/administrer/produits.php?mode=ajout\"]")
-    SelectCategory = By.NAME("categories[]")
-    Position = By.NAME("position")
+    page_title = (By.CSS_SELECTOR,'#page_title > h1')
+    SelectCategory = (By.NAME,'categories[]')
+    Position = (By.NAME,'position')
 
-
-
-
-    def __init__(self, driver):
-        super(AddProductPage, self).__init__(driver)
-
+    #Add Product
+    def AddProduct(self):
+        self.driver.find_element(*AddProductPage.admin_button).click()
+        self.driver.find_element(*AddProductPage.add_product_link).click()
+        self.assertEqual("Add product", self.driver.find_element(*AddProductPage.page_title).text)
 
 
 
--- Tests/AddProduct.py
+++ Tests/AddProduct.py
@@ -1,21 +1,10 @@
-from POM.PaymentPages import PaymentPages
-from HelpReferences.basetestcase import BaseTestCase
+from POM.BaseTestCase import BaseTestCase
 from POM.LoginPage import LoginPage
-from selenium.webdriver.common.by import By
+from POM.AddProductPage import AddProductPage
 
 
 class AddProduct(BaseTestCase):
 
-    def test_login_valid(self):
+    def Test_Add_Product(self):
         LoginPage.login_with_valid_credentials(self, 'csadek@integrant.com', 'ZAQ!cde3')
-        self.driver.find_element(By.CSS_SELECTOR, '#search_category').send_keys('Clothing')
-        self.driver.find_element(By.CSS_SELECTOR, 'span.input-group-btn > input').click()
-        PaymentPages.Pay_Oneproduct(self)
-        PaymentPages.Billing_Address(self,'Integrant','Rihan','Mohammad','eng.mohammadriihan@gmail.com','012','566st','11411'
-                                ,'FR','France','Thank you')
-
-        Screenshot = self.driver.get_screenshot_as_file('screenshot_PayConfirm.jpg')
-
-    '''def Test_Oneproduct(self):
-        Payment1.Pay_Oneproduct(self,'France','Pickup in store','Integrant','Rihan','Mohammad','eng.mohammadriihan@gmail.com','012','566st','11411'
-                                ,'FR','France','Thank you')'''
+        AddProductPage.AddProduct(self)
