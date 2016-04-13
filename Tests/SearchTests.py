2c3ebc9b8dbe11f0df98b0f5b8485a36c62b662e
csadek <ZAQ!cde3>
2016-04-13 21:20:31 +0300

    rephrase the file

---
+++ Tests/SearchTests.py
@@ -0,0 +1,13 @@
+from POM.BaseTestCase import BaseTestCase
+from POM.searchPage import SearchPage
+from POM.LoginPage import LoginPage
+
+
+class SearchTests(BaseTestCase):
+
+    def test_search_Exist_products(self):
+        LoginPage.login_with_valid_credentials(self,"csadek","ZAQ!cde3")
+        SearchPage.search_valid_Data(self,'trouser','Clothing')
+        SearchPage.get_Product_Name(self)
+        self.assertIn('TROUSER',SearchPage.get_Product_Name(self))
+
