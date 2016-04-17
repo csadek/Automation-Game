01b6571218decbe2777075061ba0d9f6ceda5fae
jihadMohamed <jmohamed@integrant.com>
2016-04-17 14:41:54 +0200

    Mange Belling

--- POM/ProductDetailsPage.py
+++ POM/ProductDetailsPage.py
@@ -1,12 +1,11 @@
 from selenium.webdriver.common.by import By
 from POM.BaseTestCase import BaseTestCase
-
+from POM.searchPage import SearchPage
 
 class ProductDetailsPage(BaseTestCase):
     """ this class represent login page elements manipulations and functions"""
 
     # Locators
-
 
     def __init__(self, driver):
         super(ProductDetailsPage, self).__init__(driver)
