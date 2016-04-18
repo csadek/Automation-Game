15ae33f89127e7e28d29894c3e26026546a3aa52
jihadMohamed <jmohamed@integrant.com>
2016-04-18 10:43:56 +0200

    Update Search.py

--- Tests/Search.py
+++ Tests/Search.py
@@ -1,12 +1,12 @@
 from POM.BaseTestCase import BaseTestCase
-from POM.SearchPage import SearchPage
-from POM.LoginLogoutPage import LoginPage
+from POM.searchPage  import SearchPage
+from POM.LoginLogoutPage import LoginLogoutPage
 
 
 class Search(BaseTestCase):
 
     def test_search_Exist_products(self):
-        LoginPage.login_with_valid_credentials(self,"csadek","ZAQ!cde3")
+        LoginLogoutPage.login_with_valid_credentials(self,"csadek","ZAQ!cde3")
         SearchPage.search_valid_Data(self,'trouser','Clothing')
         SearchPage.get_Product_Name(self)
         self.assertIn('TROUSER',SearchPage.get_Product_Name(self))
