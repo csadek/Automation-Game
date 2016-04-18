027e6733b1d1adccdc94d7ae903d59fe93e46f88
jihadMohamed <jmohamed@integrant.com>
2016-04-18 10:12:39 +0200

    ProductDetails

--- Tests/SystemTest.py
+++ Tests/SystemTest.py
@@ -1,16 +1,17 @@
 from POM.BaseTestCase import BaseTestCase
-from POM.LoginLogoutPage import LoginPage
-from POM import SearchPage
+from POM.LoginLogoutPage import LoginLogoutPage
+from POM.searchPage import SearchPage
 from POM.PaymentPages import PaymentPages
+from POM.ProductDetailsPage import ProductDetails
 
 
 class SystemTest(BaseTestCase):
 
     def test_search_Exist_products(self):
-        LoginPage.Open_Login_Page(self)
-        LoginPage.login_with_valid_credentials(self,"csadek","ZAQ!cde3")
-        SearchPage.SearchPage.search_valid_Data(self, 'trouser', 'Clothing')
-        #SearchPage.get_Product_Name(self)
+        LoginLogoutPage.login_with_valid_credentials(self,"csadek","ZAQ!cde3")
+        SearchPage.search_valid_Data(self, 'trouser', 'Clothing')
+        SearchPage.get_Product_Name(self)
+        ProductDetails.getProductDetails(self,'trouser')
         PaymentPages.Pay_Oneproduct(self)
         PaymentPages.Billing_Address(self,'Integrant','Rihan','Mohammad','eng.mohammadriihan@gmail.com','012','566st','11411'
                                 ,'FR','France','Thank you')
