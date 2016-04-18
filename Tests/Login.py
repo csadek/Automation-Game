0d9ea5ffaf375ea3fff38530acf24b5a3118d168
EngMRihan <eng.mohammadrihan@gmail.com>
2016-04-18 13:43:56 +0200

    18/4 Login with DD Done

--- Tests/Login.py
+++ Tests/Login.py
@@ -4,25 +4,24 @@ from Utilities.ReadExcel import ReadExcel
 from ddt import ddt, data, unpack
 
 @ddt
-@data(*ReadExcel.get_data('../Utilities/Data.xlsx','Login'))
 class LoginLogout(BaseTestCase):
 
-    @data(*ReadExcel.get_data('../Utilities/Data.xlsx','Login'))
+    @data(*ReadExcel.get_data('../Utilities/Data.xlsx','LoginInvalid'))
     @unpack
     def test_login_incorrect(self,one,two):
         self.assertTrue('Bad email or password.',LoginLogoutPage.login_with_Invalid_credentials(self, one,two))
 
-    @data(*ReadExcel.get_data('../Utilities/Data.xlsx','Login'))
+    @data(*ReadExcel.get_data('../Utilities/Data.xlsx','LoginBlankPW'))
     @unpack
     def test_login_blank_password(self,one,two):
         self.assertTrue('Please type in your password.',LoginLogoutPage.login_with_Invalid_credentials2(self,one,two))
 
-    @data(*ReadExcel.get_data('../Utilities/Data.xlsx','Login'))
+    @data(*ReadExcel.get_data('../Utilities/Data.xlsx','LoginBlankUN'))
     @unpack
     def test_login_blank_email(self,one,two):
         self.assertTrue('Bad email or password.',LoginLogoutPage.login_with_Invalid_credentials(self,one,two))
 
-    @data(*ReadExcel.get_data('../Utilities/Data.xlsx','Login'))
+    @data(*ReadExcel.get_data('../Utilities/Data.xlsx','LoginValid'))
     @unpack
     def test_login_valid(self,one,two):
         self.driver.get('http://10.1.22.67/Jamaica/utilisateurs/enregistrement.php')
