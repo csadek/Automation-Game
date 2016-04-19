0db600f9afa70c98c7de06414dde01123a992d7d
jihadMohamed <jmohamed@integrant.com>
2016-04-19 15:33:31 +0200

    system test after adding send Email

--- Tests/SendEmail.py
+++ Tests/SendEmail.py
@@ -1,21 +1,33 @@
 from POM.BaseTestCase import BaseTestCase
+from POM.LoginLogoutPage import LoginLogoutPage
+from POM.ProductDetailsPage import ProductDetails
 from POM.SendEmailPage import sendEmailClass
+from Utilities.ReadExcel import ReadExcel
+from ddt import ddt, data, unpack
 
-
-
+@ddt
 class sendEmailTestCases(BaseTestCase):
 
-    def test_mail_incorrect(self):
-        errorMessage ='The form content is invalid (unvalid email ou missing data).' \
-                     ' Please click "back" and fill the forms with correct answers.'
+    @data(*ReadExcel.get_data('../Automation-Game/Automation-Game/Utilities/Data.xlsx','SendEmail'))
+    @unpack
+    def test_send_mail_successfully(self,SenderName,SenderEmail,RecName,RecEmails,comment):
+        LoginLogoutPage.login_with_valid_credentials(self, 'csadek@integrant.com', 'ZAQ!cde3')
+        ProductDetails.getProductDetails(self,'trouser')
+        ProductDetails.send_email(self)
 
-        error = sendEmailClass.send_Email_missing_data(self,'csadek@integrant.com','ali','jmohamed@integrant.com')
-        self.assertTrue(errorMessage ,error)
-
-
-    def test_send_mail_successfully(self):
-        conformation_Msg = sendEmailClass.send_Email_successfully(self,'Chris','Chris@integrant.com','Jihad','jMohamed@integrant.com','Hello')
+        conformation_Msg = sendEmailClass.send_Email_successfully(self,SenderName,SenderEmail,RecName,RecEmails,comment)
         self.assertTrue('Your message has been sent.',conformation_Msg)
+
+    def test_mail_empty_data(self):
+         LoginLogoutPage.login_with_valid_credentials(self, 'csadek@integrant.com', 'ZAQ!cde3')
+         ProductDetails.getProductDetails(self,'trouser')
+         ProductDetails.send_email(self)
+
+         errorMessage = 'The form content is invalid (unvalid email ou missing data). Please click "back" and fill the forms with correct answers.'
+         self.assertTrue(errorMessage ,sendEmailClass.send_Email_empty_data(self))
+
+
+
 
 
 
