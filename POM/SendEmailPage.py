f44f3c63960fa9007c2706464a24dfd355361a7a
jihadMohamed <jmohamed@integrant.com>
2016-04-19 14:53:22 +0200

    Update SendEmailPage.py

--- POM/SendEmailPage.py
+++ POM/SendEmailPage.py
@@ -2,33 +2,36 @@ from selenium.webdriver.common.by import By
 from POM.BaseTestCase import BaseTestCase
 
 
-class sendEmail(BaseTestCase):
+class sendEmailClass(BaseTestCase):
     firstName = (By.NAME,'yname')
     yourEmail = (By.NAME,'yemail')
     recipientNames = (By.NAME,'fname[]')
     recipientEmails = (By.NAME,'femail[]')
     comments = (By.NAME,'comments')
     sendButton= (By.NAME,'action')
+    backToProduct = (By.PARTIAL_LINK_TEXT,'Go back to the referer.')
     errorMsg= (By.CSS_SELECTOR,'#main_content > div:nth-child(1) > div > div > div.middle_column_repeat > div')
     confirmationMsg=(By.CSS_SELECTOR,'#main_content > div:nth-child(1) > div > div > div.middle_column_repeat > div')
 
-    def send_Email_successfully(self,name,RecName,RecEmails ,comment):
-        self.driver.find_element(*sendEmail.firstName).send_keys(name)
-        self.driver.find_element(*sendEmail.recipientNames).send_keys(RecName)
-        self.driver.find_element(*sendEmail.recipientEmails).send_keys(RecEmails)
-        self.driver.find_element(*sendEmail.comments).send_keys(comment)
-        self.driver.find_element(*sendEmail.sendButton).click()
+    def send_Email_successfully(self,name,senderEmail,RecName,RecEmails ,comment):
+        self.driver.find_element(*sendEmailClass.firstName).send_keys(name)
+        self.driver.find_element(*sendEmailClass.yourEmail).clear()
+        self.driver.find_element(*sendEmailClass.yourEmail).send_keys(senderEmail)
+        self.driver.find_element(*sendEmailClass.recipientNames).send_keys(RecName)
+        self.driver.find_element(*sendEmailClass.recipientEmails).send_keys(RecEmails)
+        self.driver.find_element(*sendEmailClass.comments).send_keys(comment)
+        self.driver.find_element(*sendEmailClass.sendButton).click()
 
-        return self.driver.find_element(*sendEmail.confirmationMsg).text
+        return self.driver.find_element(*sendEmailClass.confirmationMsg).text
 
     def send_Email_missing_data(self,RecName,RecEmails):
-        self.driver.find_element(*sendEmail.recipientNames).send_keys(RecName)
-        self.driver.find_element(*sendEmail.recipientEmails).send_keys(RecEmails)
-        self.driver.find_element(*sendEmail.sendButton).click()
+        self.driver.find_element(*sendEmailClass.recipientNames).send_keys(RecName)
+        self.driver.find_element(*sendEmailClass.recipientEmails).send_keys(RecEmails)
+        self.driver.find_element(*sendEmailClass.sendButton).click()
 
-    def get_Error_mesage(self):
-        return self.driver.find_element(*sendEmail.errorMsg).text
-
+        return self.driver.find_element(*sendEmailClass.errorMsg).text
 
 
+    def back_to_product_Page(self):
+        self.driver.find_element(*sendEmailClass.backToProduct).click()
 
