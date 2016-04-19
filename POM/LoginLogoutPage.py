1788c7c10ac464a3834efa4f202b49a6c29e2271
csadek <ZAQ!cde3>
2016-04-20 02:24:37 +0300

    rephrase the file

--- POM/LoginLogoutPage.py
+++ POM/LoginLogoutPage.py
@@ -10,8 +10,7 @@ class LoginLogoutPage(BaseTestCase):
     username = (By.CSS_SELECTOR,'#compte_login_mini > form > table > tbody > tr:nth-child(1) > td.module_login_email > input')
     password =(By.CSS_SELECTOR,'#compte_login_mini > form > table > tbody > tr:nth-child(2) > td.module_login_password > input')
     login_button = (By.CSS_SELECTOR,'#compte_login_mini > form > table > tbody > tr:nth-child(3) > td > input.btn.btn-success')
-    Error_LBL = (By.CSS_SELECTOR, 'tr:nth-child(1) > td:nth-child(2) > div.alert.alert-danger.fade.in')
-    Error_LBL2 = (By.CSS_SELECTOR, 'tr:nth-child(2) > td:nth-child(2) > div.alert.alert-danger.fade.in')
+    Error_LBL = (By.CSS_SELECTOR, 'div[class=\'alert alert-danger fade in\']')
     #logout
     log_out_admin = (By.CSS_SELECTOR, '#total > div.navbar.navbar-inverse.navbar-static-top > div > div > div.navbar-collapse.collapse > nav > ul.nav.nav-pills.pull-right > li:nth-child(2) > a')
     logout_link = (By.CSS_SELECTOR,'#header_login > div > a > span.hidden-xs > span.header_user_text')
@@ -26,15 +25,6 @@ class LoginLogoutPage(BaseTestCase):
         self.driver.find_element(*LoginLogoutPage.login_button).click()
         return self.driver.find_element(*LoginLogoutPage.Error_LBL)
 
-    def login_with_Invalid_credentials2(self, username, password):
-        self.driver.find_element(*LoginLogoutPage.login_link).click()
-        self.driver.find_element(*LoginLogoutPage.username).clear()
-        self.driver.find_element(*LoginLogoutPage.username).send_keys(username)
-        self.driver.find_element(*LoginLogoutPage.password).clear()
-        self.driver.find_element(*LoginLogoutPage.password).send_keys(password)
-        self.driver.find_element(*LoginLogoutPage.login_button).click()
-        return self.driver.find_element(*LoginLogoutPage.Error_LBL2)
-
     def login_with_valid_credentials(self, username, password):
         self.driver.find_element(*LoginLogoutPage.login_link).click()
         self.driver.find_element(*LoginLogoutPage.username).clear()
@@ -42,6 +32,7 @@ class LoginLogoutPage(BaseTestCase):
         self.driver.find_element(*LoginLogoutPage.password).clear()
         self.driver.find_element(*LoginLogoutPage.password).send_keys(password)
         self.driver.find_element(*LoginLogoutPage.login_button).click()
+        return self.driver.find_element(*LoginLogoutPage.login_link).text
 
     def logout(self):
         if 'http://10.1.22.67/Jamaica/administrer' in self.driver.current_url:
