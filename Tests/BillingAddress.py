32e10885f07ed7783e1e5fc2226ee14c30f700d2
jihadMohamed <jmohamed@integrant.com>
2016-04-19 19:49:37 +0200

    billingAddress

--- Tests/BillingAddress.py
+++ Tests/BillingAddress.py
@@ -7,7 +7,7 @@ from ddt import ddt, data, unpack
 @ddt
 class MangeBilling(BaseTestCase):
 
-    @data(*ReadExcel.get_data('../Automation-Game/Automation-Game/Utilities/Data.xlsx','BillingAddress'))
+    @data(*ReadExcel.get_data('../Utilities/Data.xlsx','BillingAddress'))
     @unpack
     def test_Change_Billing_Address(self,surAddress,surName,firstName,email,company,address,zipCode,town,country,phone):
         LoginLogoutPage.login_with_valid_credentials(self, 'csadek@integrant.com', 'ZAQ!cde3')
