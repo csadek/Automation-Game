5f2faf3c4584be03652ee9db0949396c6d17ec48
EngMRihan <eng.mohammadrihan@gmail.com>
2016-04-19 16:32:58 +0200

    19/4

--- Tests/FollowUpOrder.py
+++ Tests/FollowUpOrder.py
@@ -1,11 +1,16 @@
 from POM.BaseTestCase import BaseTestCase
 from POM.LoginLogoutPage import LoginLogoutPage
 from POM.FollowUpOrderPage import FollowUpOrdersPage
+from Utilities.ReadExcel import ReadExcel
+from ddt import ddt, data, unpack
 
-
+@ddt
 class FollowUpOrder(BaseTestCase):
-    def test_FollowOrder_valid(self):
-        LoginLogoutPage.login_with_valid_credentials(self, 'csadek@integrant.com', 'ZAQ!cde3')
+
+    @data(*ReadExcel.get_data('../Utilities/Data.xlsx','LoginValid'))
+    @unpack
+    def test_FollowOrder_valid(self, username, password):
+        LoginLogoutPage.login_with_valid_credentials(self, username, password)
         FollowUpOrdersPage.FollowOrder(self)
         FollowUpOrdersPage.get_Status_Name(self)
         self.assertIn('Payment pending', FollowUpOrdersPage.get_Status_Name(self))
--- Tests/Payment.py
+++ Tests/Payment.py
@@ -7,13 +7,22 @@ from ddt import ddt, data, unpack
 
 @ddt
 class Payment(BaseTestCase):
+    @data(*ReadExcel.get_data('../Utilities/Data.xlsx','LoginValid'))
+    @unpack
+    def test_payment1(self,username,password):
+        LoginLogoutPage.login_with_valid_credentials(self, username,password)
+
+    @data(*ReadExcel.get_data('../Utilities/Data.xlsx','Search'))
+    @unpack
+    def test_payment2(self,item,category):
+        SearchPage.search_valid_Data(self,item,category)
+        PaymentPages.Pay_Oneproduct(self)
+
     @data(*ReadExcel.get_data('../Utilities/Data.xlsx','BillingAddress'))
     @unpack
     def test_payment(self,company,surname,name,email,phone,address,zipcode,town,country,comment):
-        LoginLogoutPage.login_with_valid_credentials(self, 'csadek@integrant.com', 'ZAQ!cde3')
-        SearchPage.search_valid_Data(self,'trouser','Clothing')
-        PaymentPages.Pay_Oneproduct(self)
         PaymentPages.Billing_Address(self,company,surname,name,email,phone,address,zipcode,town,country,comment)
         self.driver.get_screenshot_as_file('screenshot_PayConfirm.jpg')
+
         PaymentPages.get_Confirm_Msg(self)
         self.assertIn('Confirmation of your order',PaymentPages.get_Confirm_Msg(self))
