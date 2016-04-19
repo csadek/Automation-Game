5b28435c043e5406f91f644b778aa188df33b104
jihadMohamed <jmohamed@integrant.com>
2016-04-19 13:45:14 +0200

    Update SendEmail.py

--- Tests/SendEmail.py
+++ Tests/SendEmail.py
@@ -6,12 +6,15 @@ from POM.SendEmailPage import sendEmail
 class sendEmailTestCases(BaseTestCase):
 
     def test_mail_incorrect(self):
-        self.assertTrue('The form content is invalid (unvalid email ou missing data). Please click "back" and fill the forms with correct answers.'
-                        ,sendEmail.send_Email_missing_data(self,'csadek@integrant.com','ali','jmohamed@integrant.com'))
+        errorMessage ='The form content is invalid (unvalid email ou missing data).' \
+                     ' Please click "back" and fill the forms with correct answers.'
+
+        sendEmail.send_Email_missing_data(self,'csadek@integrant.com','ali','jmohamed@integrant.com')
+        self.assertTrue(errorMessage ,sendEmail.get_Error_mesage(self))
 
 
-    def test_send_mail(self):
-        conformation_Msg = sendEmail.send_Email_successfully(self,'Chris','Jihad','jMohamed@integrant.com','Hello')
+    def test_send_mail_successfully(self):
+        conformation_Msg = sendEmail.send_Email_successfully(self,'Chris','Chris@integrant.com','Jihad','jMohamed@integrant.com','Hello')
         self.assertTrue('Your message has been sent.',conformation_Msg)
 
 
