a843826fe5f53c459886543bb642d4bbdf51214e
jihadMohamed <jmohamed@integrant.com>
2016-04-19 16:03:01 +0200

    Update SendEmail.py

--- Tests/SendEmail.py
+++ Tests/SendEmail.py
@@ -8,23 +8,28 @@ from ddt import ddt, data, unpack
 @ddt
 class sendEmailTestCases(BaseTestCase):
 
-    @data(*ReadExcel.get_data('../Automation-Game/Automation-Game/Utilities/Data.xlsx','SendEmail'))
+    @data(*ReadExcel.get_data('../Utilities/Data.xlsx','SendEmail'))
     @unpack
-    def test_send_mail_successfully(self,SenderName,SenderEmail,RecName,RecEmails,comment):
+    def test_send_email_successfully(self,SenderName,SenderEmail,RecName,RecEmails,comment):
         LoginLogoutPage.login_with_valid_credentials(self, 'csadek@integrant.com', 'ZAQ!cde3')
         ProductDetails.getProductDetails(self,'trouser')
         ProductDetails.send_email(self)
 
         conformation_Msg = sendEmailClass.send_Email_successfully(self,SenderName,SenderEmail,RecName,RecEmails,comment)
         self.assertTrue('Your message has been sent.',conformation_Msg)
+        sendEmailClass.back_to_product_Page(self)
 
-    def test_mail_empty_data(self):
-         LoginLogoutPage.login_with_valid_credentials(self, 'csadek@integrant.com', 'ZAQ!cde3')
-         ProductDetails.getProductDetails(self,'trouser')
-         ProductDetails.send_email(self)
+    def test_send_empty_data(self):
+     LoginLogoutPage.login_with_valid_credentials(self, 'csadek@integrant.com', 'ZAQ!cde3')
+     ProductDetails.getProductDetails(self,'trouser')
+     ProductDetails.send_email(self)
 
-         errorMessage = 'The form content is invalid (unvalid email ou missing data). Please click "back" and fill the forms with correct answers.'
-         self.assertTrue(errorMessage ,sendEmailClass.send_Email_empty_data(self))
+     errorMessage = 'The form content is invalid (unvalid email ou missing data). Please click "back" and fill the forms with correct answers.'
+     self.assertTrue(errorMessage ,sendEmailClass.send_Email_empty_data(self))
+
+
+
+
 
 
 
