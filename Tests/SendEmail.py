9ad32bfea01c8b9d976185c75037fb4e0b144412
jihadMohamed <jmohamed@integrant.com>
2016-04-19 16:11:23 +0200

    Update SendEmail.py

--- Tests/SendEmail.py
+++ Tests/SendEmail.py
@@ -20,10 +20,7 @@ class sendEmailTestCases(BaseTestCase):
         sendEmailClass.back_to_product_Page(self)
 
     def test_send_empty_data(self):
-     LoginLogoutPage.login_with_valid_credentials(self, 'csadek@integrant.com', 'ZAQ!cde3')
-     ProductDetails.getProductDetails(self,'trouser')
      ProductDetails.send_email(self)
-
      errorMessage = 'The form content is invalid (unvalid email ou missing data). Please click "back" and fill the forms with correct answers.'
      self.assertTrue(errorMessage ,sendEmailClass.send_Email_empty_data(self))
 
