bea0288b839c4ebf58a86006b75ad928413ebeeb
jihadMohamed <jmohamed@integrant.com>
2016-04-25 23:03:34 +0200

    Delete Create Address file

--- POM/ManageBillingAddressPage.py
+++ POM/ManageBillingAddressPage.py
@@ -1,16 +1,25 @@
 from selenium.webdriver.common.by import By
 from POM.BaseTestCase import BaseTestCase
-from POM.CreateBillingAddressPage import CreateBillingAddress
 
 
 class ManageBillingAddressPage(BaseTestCase):
-    '''revisit the class'''
     PageTitle = (By.CLASS_NAME,'page_title')
     BillingAddressLink = (By.PARTIAL_LINK_TEXT,'Manage my billing and shipping addresses')
     BillingAddressList = (By.ID , 'address_default')
     ShippingAddress = (By.NAME,'personal_address_ship')
-    # should be added at create address
     CreatAddressBtn = (By.PARTIAL_LINK_TEXT ,'Create another address')
+    surnameAddress=(By.NAME,'name_adresse')
+    TypeList= (By.NAME, "address_type")   # type drop down list
+    Surname = (By.ID, 'nom_famille')
+    FirstName = (By.ID, 'prenom')
+    Email = (By.ID,'email')
+    Company = (By.ID, 'societe')
+    Address = (By.ID, 'adresse')
+    Zipcode = (By.ID, 'code_postal')
+    Town = (By.ID, 'ville')
+    Country = select_Country = (By.ID, "pays")
+    Phone = (By.ID, 'portable')
+    ValidateBtn = (By.CSS_SELECTOR ,'#main_content > div:nth-child(1) > div > div > div.middle_column_repeat > form > fieldset > p > input' )
 
     def manage_billing(self):
         self.driver.find_element(*ManageBillingAddressPage.BillingAddressLink).click()
@@ -18,11 +27,18 @@ class ManageBillingAddressPage(BaseTestCase):
 
     def Create_Another_Address(self,surAddress,surName,firstName,email,company,address,zipCode,town,country,phone):
         self.driver.find_element(*ManageBillingAddressPage.CreatAddressBtn).click()
-        CreateBillingAddress.new_address_vaild_input(self,surAddress,surName,firstName,email,company,address,zipCode,town,country,phone)
+        self.driver.find_element(*ManageBillingAddressPage.surnameAddress).send_keys(surAddress)
+        self.driver.find_element(*ManageBillingAddressPage.Surname).send_keys(surName)
+        self.driver.find_element(*ManageBillingAddressPage.FirstName).send_keys(firstName)
+        self.driver.find_element(*ManageBillingAddressPage.Email).send_keys(email)
+        self.driver.find_element(*ManageBillingAddressPage.Company).send_keys(company)
+        self.driver.find_element(*ManageBillingAddressPage.Address).send_keys(address)
+        self.driver.find_element(*ManageBillingAddressPage.Zipcode).send_keys(zipCode)
+        self.driver.find_element(*ManageBillingAddressPage.Town).send_keys(town)
+        self.driver.find_element(*ManageBillingAddressPage.Country).send_keys(country)
+        self.driver.find_element(*ManageBillingAddressPage.Phone).send_keys(phone)
+
+        self.driver.find_element(*ManageBillingAddressPage.ValidateBtn).click()
 
     def __init__(self, driver):
         super(ManageBillingAddressPage, self).__init__(driver)
-
-
-
-
