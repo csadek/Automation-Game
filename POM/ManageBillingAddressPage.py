9c9af5d913721d39d8d466ced4ba4a1205d0a5c0
jihadMohamed <jmohamed@integrant.com>
2016-05-09 10:55:31 +0200

    Delete Create Address file

--- POM/ManageBillingAddressPage.py
+++ POM/ManageBillingAddressPage.py
@@ -1,12 +1,15 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+
 from POM.BaseTestCase import BaseTestCase
 
 
 class ManageBillingAddressPage(BaseTestCase):
     PageTitle = (By.CLASS_NAME,'page_title')
     BillingAddressLink = (By.PARTIAL_LINK_TEXT,'Manage my billing and shipping addresses')
-    BillingAddressList = (By.ID , 'address_default')
-    ShippingAddress = (By.NAME,'personal_address_ship')
+    billingAddressList = (By.NAME , 'personal_address_bill')
+    shippingAddressList = (By.NAME,'personal_address_ship')
+    confirm_delete = (By.CSS_SELECTOR,'body > div.bootbox.modal.fade.in > div > div > div.modal-footer > button.btn.btn-primary')
 
     #Create Another address Elements
     CreatAddressBtn = (By.PARTIAL_LINK_TEXT ,'Create another address')
@@ -23,6 +26,7 @@ class ManageBillingAddressPage(BaseTestCase):
     Phone = (By.ID, 'portable')
     ValidateBtn = (By.CSS_SELECTOR ,'#main_content > div:nth-child(1) > div > div > div.middle_column_repeat > form > fieldset > p > input' )
     createAddressMsg = (By.CSS_SELECTOR,'#main_content > div:nth-child(1) > div > div > div.middle_column_repeat > div.alert.alert-success.fade.in')
+
 
     def manage_billing(self):
         self.driver.find_element(*ManageBillingAddressPage.BillingAddressLink).click()
@@ -43,12 +47,21 @@ class ManageBillingAddressPage(BaseTestCase):
 
         self.driver.find_element(*ManageBillingAddressPage.ValidateBtn).click()
 
-
     def get_surName_address(self):
-        return  self.driver.find_element(*ManageBillingAddressPage.surnameAddress).text
+        return self.driver.find_element(*ManageBillingAddressPage.surnameAddress).text
 
     def get_createAddress_msg(self):
         return self.driver.find_element(*ManageBillingAddressPage.createAddressMsg).text
+
+
+    def get_billingAddress_list(self):
+        billingaddress_list= Select( self.driver.find_element(*ManageBillingAddressPage.billingAddressList))
+        return billingaddress_list
+
+    def get_shippingAddress_list(self):
+        shippingAddress_list= Select( self.driver.find_element(*ManageBillingAddressPage.shippingAddressList))
+        return shippingAddress_list
+
 
 
     def __init__(self, driver):
