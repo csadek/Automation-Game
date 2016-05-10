1b8417fa25845215d2e3e13d366adb41f3161df9
csadek <ZAQ!cde3>
2016-05-10 18:32:19 +0200

    rephrase the file

--- Archive/LogoutPage.py
+++
@@ -1,9 +0,0 @@
-from selenium.webdriver.common.by import By
-from POM.BaseTestCase import BaseTestCase
-
-
-class LogoutPage(BaseTestCase):
-    Logout_Btn = (By.CSS_SELECTOR,'a.btn.btn-primary.pull-right')
-
-    def logout(self):
-        self.driver.find_element(*LogoutPage.Logout_Btn).click()
--- Archive/login.py
+++
@@ -1,5 +0,0 @@
-login_data = {
-    "username": "x@gmail.com",
-    "id": 200
-}
-
