f28b8c219f83bd84974dd1d06deba1531c986264
csadek <ZAQ!cde3>
2016-05-27 22:59:47 +0200

    rephrase the file

--- POM/Administrator/CategoryPage.py
+++ POM/Administrator/CategoryPage.py
@@ -5,44 +5,46 @@ from POM.Administrator.AdminBase import AdminBase
 class CategoryPage(AdminBase):
     """ this class represents add and delete both category and subcategory elements manipulations and functions"""
     """ Admin user should add categories and subcategories to add products to those categories"""
-
     # Locators
-    root_category = (By.CSS_SELECTOR,'#total > div.container > div > div > form > table > tbody > tr:nth-child(3) > td:nth-child(2) > select > option:nth-child(1)')
-    view_on_home = (By.CSS_SELECTOR,'#total > div.container > div > div > form > table > tbody > tr:nth-child(5) > td:nth-child(2) > input[type="checkbox"]')
+    root_category = (By.CSS_SELECTOR, 'option[value="0"]')
+    view_on_home = (By.NAME, 'on_special')
     state_online = (By.NAME, 'etat')
-    name = (By.NAME,'nom_en')
-    parent = (By.CSS_SELECTOR,'#total > div.container > div > div > form > table > tbody > tr:nth-child(3) > td:nth-child(2) > select > option[value]')
-    add_category_button = (By.CSS_SELECTOR,'#total > div.container > div > div > form > table > tbody > tr:nth-child(27) > td > p > input')
+    name = (By.NAME, 'nom_en')
+    parents = (By.CSS_SELECTOR, 'option[value]')
+    add_category_button = (By.CSS_SELECTOR, 'input[class="alert alert-success fade in"]')
+    list_of_categories_name = (By.CSS_SELECTOR, 'td:nth-child(4) a')
     # alert
-    alert = (By.CSS_SELECTOR,'div[class=\'alert alert-success fade in\']')
+    alert = (By.CSS_SELECTOR, 'div[class="alert alert-success fade in"]')
     # category online
-    view_online = (By.CSS_SELECTOR, '#total > div.container > div > div > form > table > tbody > tr:nth-child(1) > td > a')
-    title = (By.CSS_SELECTOR,'#main_content > div > div > div > div.middle_column_repeat > div:nth-child(2) > h1')
+    view_online = (By.CSS_SELECTOR, 'td a')
+    title = (By.CSS_SELECTOR, 'h1[class="products_list_brief"]')
+
+    # constructor
+    def __init__(self, driver):
+        super(CategoryPage, self.__init__(driver))
 
     # Add category or sub category
-    def add_category(self,name,parent):
+    def add_category(self,username,password,categoryname, parent):
         # Open category page
-        self.driver.find_element(*CategoryPage.main_menu).click()
-        self.driver.find_element(*CategoryPage.sub_menu).click()
-        self.driver.find_element(*CategoryPage.add_category_link).click()
-        # Add sub category
+        AdminBase.navigate_to_admin(self,username, password)
+        AdminBase.add_category_link(self)
         if parent == "":
+            #  Add category
             self.driver.find_element(*CategoryPage.root_category).click()
         else:
-            for i in self.driver.find_elements(*CategoryPage.parent):
+            # Add sub category
+            for i in self.driver.find_elements(*CategoryPage.parents):
                 if i.text == parent:
                     i.click()
         self.driver.find_element(*CategoryPage.view_on_home).click()
         self.driver.find_element(*CategoryPage.state_online).click()
-        self.driver.find_element(*CategoryPage.name).send_keys(name)
+        self.driver.find_element(*CategoryPage.name).send_keys(categoryname)
         # submit
         self.driver.find_element(*CategoryPage.add_category_button).click()
 
         # Open edit category page
-        self.driver.find_element(*CategoryPage.main_menu).click()
-        self.driver.find_element(*CategoryPage.sub_menu).click()
-        self.driver.find_element(*CategoryPage.category_list_link).click()
-        self.driver.find_element_by_link_text(name).click()
+        AdminBase.edit_category_navigator(self)
+        self.driver.find_element_by_link_text(categoryname).click()
 
         # view online
         self.driver.find_element(*CategoryPage.view_online).click()
@@ -54,23 +56,16 @@ class CategoryPage(AdminBase):
         self.driver.close()
         self.driver.switch_to_window(window_before)
         # Check category name
-        if category_page_title == name:
+        if category_page_title == categoryname:
             return True
         else:
             return False
 
     # delete category
-    def delete_category(self,idd):
+    def delete_category(self, name):
         # Open category page
-        self.driver.find_element(*CategoryPage.main_menu).click()
-        self.driver.find_element(*CategoryPage.sub_menu).click()
-        self.driver.find_element(*CategoryPage.category_list_link).click()
+        AdminBase.edit_category_navigator(self)
         # delete category
-        delete_row = (By.CSS_SELECTOR,'#total > div.container > div > div > div.table-responsive > table > tbody > tr')
-        self.driver.find_element(By.CSS_SELECTOR,'a[href="http://10.1.22.67/Jamaica/administrer/categories.php?mode=suppr&id={}"]'.format(idd))
-
-
-
-
-
-
+        for i in self.driver.find_elements(*CategoryPage.list_of_categories_name):
+            if i.text == name:
+                self.driver.find_element(By.CSS_SELECTOR,'a[title="Delete Category"]'+i)
